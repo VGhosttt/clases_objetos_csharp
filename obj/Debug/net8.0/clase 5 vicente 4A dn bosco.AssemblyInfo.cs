6f0b3b1bdb1470d33d6afa1bd1310a51c4f5a163
VGhosttt <v.hinojosaalvarez@gmail.com>
2025-08-22 17:28:56 -0400

    clase auto y objeto

--- obj/Debug/net8.0/clase 5 vicente 4A dn bosco.AssemblyInfo.cs
+++ obj/Debug/net8.0/clase 5 vicente 4A dn bosco.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clase 5 vicente 4A dn bosco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0cb22cd006334aa8516c0cc9704111ccaa7d64")]
 [assembly: System.Reflection.AssemblyProductAttribute("clase 5 vicente 4A dn bosco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clase 5 vicente 4A dn bosco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
